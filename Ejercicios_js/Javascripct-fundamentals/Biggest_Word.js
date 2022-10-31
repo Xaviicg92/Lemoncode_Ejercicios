2ea0f6b147cc25d1bd3a5932d11a20f88ff61ef1
Javier Cambón <cg_javi@hotmail.com>
2022-10-31 17:58:39 +0100

    correcion ejercicio

--- Ejercicios_js/Javascripct-fundamentals/Biggest_Word.js
+++ Ejercicios_js/Javascripct-fundamentals/Biggest_Word.js
@@ -1,30 +1,21 @@
 
 
 function biggestWord (phrase) {
-    //var frase = phrase.replace(/[,]/g, "");
-    var frase = phrase.split('');
-    var palabra = "";
-    var palabras = [];
-    
-    for (let i = 0; i <= frase.length; i++) {
-         if (frase[i] != " " && i != frase.length) {
-            palabra = palabra + frase[i];
-         } else {
-            palabras.push(palabra);
-            palabra = "";
-         }
-         
+  frase = frase.split(/[,\s]+/);
+  
+  var palabraMasLarga = "";
+  for (let i = 0; i < frase.length; i++) {
+    if (frase[i].length > palabraMasLarga.length) {
+      palabraMasLarga = frase[i];
     }
+  }
 
-    var palabraMasLarga = "";
-    for (let i = 0; i < palabras.length; i++) {
-        if (palabras[i].length > palabraMasLarga.length) {
-            palabraMasLarga = palabras[i];
-        }
-    }
-
-    return palabraMasLarga;
+  return palabraMasLarga;
 }
-var frase = "Estoy estudiando en el campus Lemoncode";
+
+var frase = "Estoy estudiando,,,,, en el campus Lemoncode";
 palabra = biggestWord(frase);
+
+
+
 console.log("La palabra más larga de esta frase es: " + palabra);
