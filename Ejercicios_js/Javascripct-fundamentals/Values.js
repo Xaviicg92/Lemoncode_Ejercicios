09f2adac12c7098aabc4ab58714a82af412d3136
Javier Cambón <cg_javi@hotmail.com>
2022-11-01 16:44:08 +0100

    Ejercicios

---
+++ Ejercicios_js/Javascripct-fundamentals/Values.js
@@ -0,0 +1,26 @@
+// function values(object) {
+
+//   var properties = [];
+//   for (const property in object) {
+//     properties.push(object[property]);
+//   }
+//   return properties;
+// }
+  
+function values(obj) {
+  var object = Object.values(obj);
+  return object;
+}
+
+function Person(name, edad) {
+  this.name = name;
+  this.edad = edad;
+}
+
+Person.prototype.walk = function() {
+  console.log("I'm walking");
+};
+
+var john = new Person("John", 25);
+console.log(john);
+console.log(values(john));
